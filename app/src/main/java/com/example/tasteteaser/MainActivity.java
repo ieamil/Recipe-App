5286f5b89681c6bd6bec5afa10b087ec44ebc38f
U200709052 <isillimaei@gmail.com>
2023-11-10 00:44:07 +0300

    login screen / animations

---
+++ app/src/main/java/com/example/tasteteaser/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.tasteteaser;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH_SCREEN = 3000;
+    private Animation topAnim;
+    private Animation bottomAnim;
+    private ImageView image;
+    private TextView logo;
+    private TextView slogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        // Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        // Hooks
+        image = findViewById(R.id.imageView2);
+        logo = findViewById(R.id.textView);
+        slogan = findViewById(R.id.textView2);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                // Call next screen
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                // Attach all the elements those you want to animate in design
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View, String>(image, "logo_image");
+                pairs[1] = new Pair<View, String>(logo, "logo_text");
+                // Wrap the call in API level 21 or higher
+                if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
+                    startActivity(intent, options.toBundle());
+                }
+            }
+        }, SPLASH_SCREEN);
+
+    }
+}
---
+++ app/src/main/java/com/example/tasteteaser/SignUp.java
@@ -0,0 +1,17 @@
+package com.example.tasteteaser;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.tasteteaser.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Kullanıcı kaydı işlevselliğini buraya ekleyin
+    }
+}
