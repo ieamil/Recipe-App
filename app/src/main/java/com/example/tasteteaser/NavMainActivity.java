d5f171f33ec7de1fa2ce46d6ba3246bf0f5c40fd
U200709052 <isillimaei@gmail.com>
2023-12-21 01:01:31 +0300

    Add NavMainActivity.java

--- app/src/main/java/com/example/tasteteaser/NavMainActivity.java
+++ app/src/main/java/com/example/tasteteaser/NavMainActivity.java
@@ -1,11 +1,14 @@
 package com.example.tasteteaser;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.appbar.MaterialToolbar;
@@ -19,19 +22,47 @@ public class NavMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
-        //for navigation drawer menu
         setContentView(R.layout.nav_activity_main);
+
         drawerLayout = findViewById(R.id.drawerLayout);
         materialToolbar = findViewById(R.id.materialToolBar);
         frameLayout = findViewById(R.id.frameLayout);
         navigationView = findViewById(R.id.navigationView);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 NavMainActivity.this, drawerLayout, materialToolbar, R.string.close_nav, R.string.open_nav);
         drawerLayout.addDrawerListener(toggle);
 
-        materialToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener());
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.nav_home) {
+                    Toast.makeText(NavMainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                    drawerLayout.closeDrawer(GravityCompat.START);}
+
+                else if (item.getItemId() == R.id.nav_profile) {
+                    Toast.makeText(NavMainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+                    drawerLayout.closeDrawer(GravityCompat.START);}
+
+                else if (item.getItemId() == R.id.nav_recipe) {
+                Toast.makeText(NavMainActivity.this, "Add Recipe", Toast.LENGTH_SHORT).show();
+                drawerLayout.closeDrawer(GravityCompat.START);}
+
+                else if (item.getItemId() == R.id.nav_recorded) {
+                    Toast.makeText(NavMainActivity.this, "Recorded", Toast.LENGTH_SHORT).show();
+                    drawerLayout.closeDrawer(GravityCompat.START);}
+
+                else if (item.getItemId() == R.id.nav_logout) {
+                    Toast.makeText(NavMainActivity.this, "Logout", Toast.LENGTH_SHORT).show();
+                    drawerLayout.closeDrawer(GravityCompat.START);
+
+                }
+                return false;
+            }
+        });
     }
 }
