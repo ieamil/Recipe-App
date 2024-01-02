fe39df4eff32aef7fece31c54ca56e55852b138a
U200709052 <isillimaei@gmail.com>
2024-01-03 01:32:30 +0300

    realtime database connection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,6 @@ android {
 }
 
 dependencies {
-    implementation ("com.google.firebase:firebase-bom-32.7.0")
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation ("com.google.android.gms:play-services-auth:19.2.0")
@@ -52,14 +50,15 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     //firebase
-
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-database")
-    implementation("com.google.firebase:firebase-auth:22.2.0")
-    implementation("com.google.firebase:firebase-analytics-ktx:21.5.0")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-analytics")
 
 
     //circle image view
     implementation ("de.hdodenhof:circleimageview:3.1.0")
-    implementation ("com.github.bumptech.glide:glide:4.11.0")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.11.0")
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.15.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.15.0") //hata çıkarsa 4.11 yap geri
 }
